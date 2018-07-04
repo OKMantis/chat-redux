72f70878d3faab1453902a8eb64b8216ca6921a2
Owen Moss <owenkmoss@gmail.com>
2018-07-04 21:16:08 +0200

    shit

--- src/reducers/messages_reducer.js
+++ src/reducers/messages_reducer.js
@@ -1,5 +1,12 @@
-export default function(state = null, action) {
+export default function messagesReducer(state = null, action) {
   switch (action.type) {
+    case 'FETCH_MESSAGES':
+      const messages = action.payload.messages;
+      const channel = action.payload.channel;
+      return messages;
+    case 'MESSAGE_POSTED':
+      console.log(action, state);
+      return state;
     default:
       return state;
   }
