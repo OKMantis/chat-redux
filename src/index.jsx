d59e54a6dd0881319239124e590bd943d8cb1a33
Owen Moss <owenkmoss@gmail.com>
2018-07-04 20:36:26 +0200

    added logger

--- src/index.jsx
+++ src/index.jsx
@@ -2,7 +2,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
-import { createStore, combineReducers } from 'redux';
+import { createStore, combineReducers, applyMiddleware } from 'redux';
+import { logger } from 'redux-logger';
 
 // internal modules
 import App from './components/app';
@@ -21,6 +22,7 @@ const reducers = combineReducers({
  selectedChannel: selectedChannelReducer
 });
 
+const middlewares = applyMiddleware(logger);
 
 const initialState = {
   messages: [],
@@ -31,7 +33,7 @@ const initialState = {
 
 // render an instance of the component in the DOM
 ReactDOM.render(
-  <Provider store={createStore(reducers)}>
+  <Provider store={createStore(reducers, {}, middlewares)}>
     <App />
   </Provider>,
   document.getElementById('root')
